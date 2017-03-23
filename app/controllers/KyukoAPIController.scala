36dc0bf6bb3f2a25e7ba583293b96845bef731bf
benevolent0505 <mikio.fjt@gmail.com>
2017-03-24 07:27:07 +0900

    現在の休講情報を取得するエンドポイントを作成した

--- app/controllers/KyukoAPIController.scala
+++ app/controllers/KyukoAPIController.scala
@@ -1,8 +1,9 @@
 package controllers
 
-import models.KyukoDate
+import models._
 import org.joda.time.LocalDateTime
 import play.api.Logger
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json._
 import play.api.mvc.{Action, Controller}
 import services.KyukoService
@@ -33,6 +34,33 @@ class KyukoAPIController extends Controller {
     )
   }
 
+  implicit val teacherToJson = new Writes[Teacher] {
+    def writes(teacher: Teacher) = Json.obj(
+      "id" -> teacher.id,
+      "name" -> teacher.name
+    )
+  }
+
+  implicit val lectureToJson = new Writes[Lecture] {
+    def writes(lecture: Lecture) = Json.obj(
+      "id" -> lecture.id,
+      "name" -> lecture.name,
+      "teacher" -> lecture.teacher.get,
+      "category" -> lecture.category,
+      "period" -> lecture.period,
+      "isGraduate" -> lecture.isGraduate
+    )
+  }
+
+  implicit val kyukoDateToJson = new Writes[KyukoDate] {
+    def writes(kyukoDate: KyukoDate) = Json.obj(
+      "id" -> kyukoDate.id,
+      "lecture" -> kyukoDate.lecture,
+      "date" -> kyukoDate.date.toString,
+      "remark" -> kyukoDate.remark
+    )
+  }
+
   // カレンダー表示用のAPIエンドポイントなのでエラーはログに残したいが穏便に処理はしたい
   def getKyukoEvents(start: Option[String], end: Option[String]) = Action {
     def parseLocalDateTime(datetime: String): Either[IllegalArgumentException, LocalDateTime] = {
@@ -55,5 +83,16 @@ class KyukoAPIController extends Controller {
     val events = KyukoService.findByRange(startTime, endTime).map(Event.apply)
 
     Ok(Json.toJson(events))
+  }
+
+  def getCurrentKyuko(schoolStr: Option[String]) = Action.async {
+    val school = schoolStr match {
+      case Some("graduate") => GraduateSchool
+      case _ => UndergraduateSchool
+    }
+
+    val f = KyukoService.fetch(school)
+
+    f.map { list => Ok(Json.toJson(list.map(_._3))) }
   }
 }
